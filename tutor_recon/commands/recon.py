8ba83d24a923656f70ffe9ce9b32f94e203b730f
thomas-skillup <thomass@skillup.tech>
2021-08-18 19:26:31 +0000

    Add 'tutor config save' step to 'tutor recon init'.

--- tutor_recon/commands/recon.py
+++ tutor_recon/commands/recon.py
@@ -16,6 +16,11 @@ from tutor_recon.util.cli import emit
 from tutor_recon.util.paths import overrides_path, root_dirs
 from tutor_recon.commands.tutor import tutor_config_save
 
+CONFIG_SAVE_STYLED = click.style("tutor config save", fg="blue")
+
+def run_tutor_config_save(context: cloup.Context) -> None:
+    emit(f"Running '{CONFIG_SAVE_STYLED}'.")
+    context.invoke(tutor_config_save)
 
 @cloup.group(
     help="Allows overriding and rendering settings, files, and templates (in their entirety) from a central location."
@@ -36,8 +41,9 @@ def recon():
     help="Reset the environment overrides directory location to default (has no effect if the directory is already in the default location).",
     is_flag=True,
 )
+@cloup.option("--tutor/--no-tutor", is_flag=True, default=True, help="Run/don't run 'tutor config save' prior to applying overrides.")
 @cloup.pass_context
-def init(context: cloup.Context, env_dir=None, reset_location=False):
+def init(context: cloup.Context, env_dir, reset_location, tutor):
     tutor_root = Path(context.obj.root)
     if reset_location:
         overrides_path(tutor_root=tutor_root).unlink(missing_ok=True)
@@ -45,6 +51,8 @@ def init(context: cloup.Context, env_dir=None, reset_location=False):
         emit(f"Set the new environment overrides location to {recon_root}.")
     else:
         recon_root = overrides_path(tutor_root=tutor_root, env_dir=env_dir).resolve()
+    if tutor:
+        run_tutor_config_save(context)
     scaffold_all(tutor_root, recon_root)
     emit(
         f"You're all set! Your environment overrides can be configured at '{recon_root}'."
@@ -59,13 +67,12 @@ def printroot(context: cloup.Context):
 
 
 @recon.command(help="Apply all override settings to the rendered environment.")
-@cloup.option("--tutor/--no-tutor", is_flag=True, default=True)
+@cloup.option("--tutor/--no-tutor", is_flag=True, default=True, help="Run/don't run 'tutor config save' prior to applying overrides.")
 @cloup.pass_context
 def save(context: cloup.Context, tutor):
     tutor_root, recon_root = root_dirs(context)
     if tutor:
-        emit("Running 'tutor config save'.")
-        context.invoke(tutor_config_save)
+        run_tutor_config_save(context)
     emit("Applying overrides.")
     override_all(tutor_root, recon_root)
 
