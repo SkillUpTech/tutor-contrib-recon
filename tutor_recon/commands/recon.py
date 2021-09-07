71d48c3a62851eab8774f28f6d777dabbb5a0bf4
Thomas Schweich <79879948+thomas-skillup@users.noreply.github.com>
2021-09-07 16:52:13 -0700

    Support template overrides, add dev script (#2)
    * Merge template override and dev script features into dev.
    
    * Squash and merge devscript_tweaks into dev.

--- tutor_recon/commands/recon.py
+++ tutor_recon/commands/recon.py
@@ -1,22 +1,23 @@
 """The Recon CLI definitions."""
 
-import json
 from pathlib import Path
-from tutor_recon.util.vjson import VJSONEncoder
+from tutor_recon.config.templates import TemplateOverride
 
 import click
 import cloup
 
 from tutor_recon.config.main import (
-    get_all_mappings,
+    main_config,
     override_all,
     scaffold_all,
 )
 from tutor_recon.util.cli import emit
 from tutor_recon.util.paths import overrides_path, root_dirs
+from tutor_recon.util import vjson
 from tutor_recon.commands.tutor import tutor_config_save
 
 CONFIG_SAVE_STYLED = click.style("tutor config save", fg="blue")
+RECON_SAVE_STYLED = click.style("tutor recon save", fg="magenta")
 
 
 def run_tutor_config_save(context: cloup.Context) -> None:
@@ -39,28 +40,18 @@ def recon():
     default=None,
 )
 @cloup.option(
-    "--reset-location",
-    help="Reset the environment overrides directory location to default (has no effect if the directory is already in the default location).",
-    is_flag=True,
-)
-@cloup.option(
     "--tutor/--no-tutor",
     is_flag=True,
     default=True,
     help="Run/don't run 'tutor config save' prior to applying overrides.",
 )
 @cloup.pass_context
-def init(context: cloup.Context, env_dir, reset_location, tutor):
+def init(context: cloup.Context, env_dir, tutor):
     tutor_root = Path(context.obj.root)
-    if reset_location:
-        overrides_path(tutor_root=tutor_root).unlink(missing_ok=True)
-        recon_root = overrides_path(tutor_root=tutor_root)
-        emit(f"Set the new environment overrides location to {recon_root}.")
-    else:
-        recon_root = overrides_path(tutor_root=tutor_root, env_dir=env_dir).resolve()
+    recon_root = overrides_path(tutor_root=tutor_root, env_dir=env_dir).resolve()
     if tutor:
         run_tutor_config_save(context)
-    scaffold_all(tutor_root, recon_root)
+    scaffold_all(recon_root)
     emit(
         f"You're all set! Your environment overrides can be configured at '{recon_root}'."
     )
@@ -89,22 +80,27 @@ def save(context: cloup.Context, tutor):
     override_all(tutor_root, recon_root)
 
 
-@recon.command(
-    help="Echo all settings, with overrides applied, over stdout in JSON format."
-)
-@cloup.option(
-    "--all",
-    default=False,
-    help="Show the complete mapping of all overrides for all files.",
-    is_flag=True,
-)
+@recon.command(help="Scaffold an override of a tutor template in its entirety.")
+@cloup.argument("path")
 @cloup.pass_context
-def list(context: cloup.Context, all: bool):
-    tutor_root, recon_root = root_dirs(context)
-    print(
-        json.dumps(
-            get_all_mappings(tutor_root, recon_root),
-            indent=4,
-            cls=VJSONEncoder.make_encoder(recon_root, expand_remote_mappings=all),
-        )
+def replace_template(context: cloup.Context, path: str):
+    _, recon_root = root_dirs(context)
+    main = main_config(recon_root)
+    override = TemplateOverride.for_template(path, recon_root)
+    main.add_override(override)
+    override.scaffold(recon_root)
+    main.save(recon_root / "main.v.json")
+    path_styled = click.style(str(path), fg="blue")
+    emit(f"Scaffolded {path_styled} at {Path(override.src).resolve()} 👍.")
+    emit(
+        f"Change the file to your heart's content, then it will be rendered when you run {RECON_SAVE_STYLED}."
     )
+
+
+@recon.command(help="Print the current recon configuration as JSON.")
+@cloup.pass_context
+def list(context: cloup.Context):
+    _, recon_root = root_dirs(context)
+    main = main_config(recon_root)
+    config_str = vjson.dumps(main, expand_remote_mappings=True)
+    print(config_str)
